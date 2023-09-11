42c001f9536dfcc053e8479b8c8157171cd74a72
= <aymancoom3@gmail.com>
2023-09-11 16:57:01 +0300

    g

--- AyDjanRepo/urls.py
+++ AyDjanRepo/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home-page"),
-    path('home/', views.home, name='home'),
+    path('', login_required(views.home), name="home-page"),
+    path('home/', login_required(views.home), name='home'),
+    path('success/', login_required(views.home), name='success_page_name'),
     path('register/', views.register, name='register'),
-    path('login/', views.custom_login, name='login'),
-    path('success/', views.home, name='success_page_name'),
+    path('accounts/login/', views.custom_login, name='login'),
 ]
