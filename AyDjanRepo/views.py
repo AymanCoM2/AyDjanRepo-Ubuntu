6d338dfd8d092424cf4d88fb996ff87239254578
Ayman <ayman.salah@2coom.com>
2024-03-05 15:28:50 +0200

    VM credits

--- AyDjanRepo/views.py
+++ AyDjanRepo/views.py
@@ -279,26 +279,26 @@ def QueryData(query, cardcode, dbParameter):
     cnxn_str = ""
 
     if (dbParameter == "TM"):
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=TM;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
     elif (dbParameter == "LB"):
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=LB;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
     else:
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=TM;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
@@ -508,26 +508,26 @@ def QueryDataALL(query , dbParameter):
     cnxn_str = ""
 
     if (dbParameter == "TM"):
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=TM;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
     elif (dbParameter == "LB"):
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=LB;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
     else:
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=TM;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
@@ -726,26 +726,26 @@ def QueryDataB(query,Foreignname,dbParameter):
     cnxn_str = ""
 
     if (dbParameter == "TM"):
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=TM;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
     elif (dbParameter == "LB"):
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=LB;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
     else:
-        cnxn_str = ("Driver={SQL Server};"
-        # cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
-                    # "Server=10.10.10.100;"
-                    "Server=jdry1.ifrserp.net,445;"
+        # cnxn_str = ("Driver={SQL Server};"
+        cnxn_str = ("Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1};"
+                    "Server=10.10.10.100;"
+                    # "Server=jdry1.ifrserp.net,445;"
                     "Database=TM;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
